afb8879ef274c07591eebf1e984725dc3747649e
ktoyod <ytdh7834@gmail.com>
2019-01-09 14:48:51 +0900

    draw joints without confidence 0 joints

--- src/openposedraw.py
+++ src/openposedraw.py
@@ -4,40 +4,30 @@ from PIL import Image, ImageDraw
 
 # draw the lines
 def drawLines(draw, joints, clr, line_width):
-    draw.line((joints[1, 0], joints[1, 1], joints[2, 0], joints[2, 1]), fill=(
-        clr[0, 0], clr[0, 1], clr[0, 2]), width=line_width)
-    draw.line((joints[1, 0], joints[1, 1], joints[5, 0], joints[5, 1]), fill=(
-        clr[1, 0], clr[1, 1], clr[1, 2]), width=line_width)
-    draw.line((joints[2, 0], joints[2, 1], joints[3, 0], joints[3, 1]), fill=(
-        clr[2, 0], clr[2, 1], clr[2, 2]), width=line_width)
-    draw.line((joints[3, 0], joints[3, 1], joints[4, 0], joints[4, 1]), fill=(
-        clr[3, 0], clr[3, 1], clr[3, 2]), width=line_width)
-    draw.line((joints[5, 0], joints[5, 1], joints[6, 0], joints[6, 1]), fill=(
-        clr[4, 0], clr[4, 1], clr[4, 2]), width=line_width)
-    draw.line((joints[6, 0], joints[6, 1], joints[7, 0], joints[7, 1]), fill=(
-        clr[5, 0], clr[5, 1], clr[5, 2]), width=line_width)
-    draw.line((joints[1, 0], joints[1, 1], joints[8, 0], joints[8, 1]), fill=(
-        clr[6, 0], clr[6, 1], clr[6, 2]), width=line_width)
-    draw.line((joints[8, 0], joints[8, 1], joints[9, 0], joints[9, 1]), fill=(
-        clr[7, 0], clr[7, 1], clr[7, 2]), width=line_width)
-    draw.line((joints[9, 0], joints[9, 1], joints[10, 0], joints[10, 1]), fill=(
-        clr[8, 0], clr[8, 1], clr[8, 2]), width=line_width)
-    draw.line((joints[1, 0], joints[1, 1], joints[11, 0], joints[11, 1]), fill=(
-        clr[9, 0], clr[9, 1], clr[9, 2]), width=line_width)
-    draw.line((joints[11, 0], joints[11, 1], joints[12, 0], joints[12, 1]), fill=(
-        clr[10, 0], clr[10, 1], clr[10, 2]), width=line_width)
-    draw.line((joints[12, 0], joints[12, 1], joints[13, 0], joints[13, 1]), fill=(
-        clr[11, 0], clr[11, 1], clr[11, 2]), width=line_width)
-    draw.line((joints[0, 0], joints[0, 1], joints[1, 0], joints[1, 1]), fill=(
-        clr[12, 0], clr[12, 1], clr[12, 2]), width=line_width)
-    draw.line((joints[0, 0], joints[0, 1], joints[14, 0], joints[14, 1]), fill=(
-        clr[13, 0], clr[13, 1], clr[13, 2]), width=line_width)
-    draw.line((joints[15, 0], joints[15, 1], joints[17, 0], joints[17, 1]), fill=(
-        clr[14, 0], clr[14, 1], clr[14, 2]), width=line_width)
-    draw.line((joints[0, 0], joints[0, 1], joints[15, 0], joints[15, 1]), fill=(
-        clr[15, 0], clr[15, 1], clr[15, 2]), width=line_width)
-    draw.line((joints[0, 0], joints[0, 1], joints[15, 0], joints[15, 1]), fill=(
-        clr[16, 0], clr[16, 1], clr[16, 2]), width=line_width)
+    def drawLine(first_idx, second_idx, clr_idx):
+        if joints[first_idx, 2] and joints[second_idx, 2]:
+            draw.line((joints[first_idx, 0], joints[first_idx, 1],
+                       joints[second_idx, 0], joints[second_idx, 1]),
+                      fill=(clr[clr_idx, 0], clr[clr_idx, 1], clr[clr_idx, 2]),
+                      width=line_width)
+
+    drawLine(1, 2, 0)
+    drawLine(1, 5, 1)
+    drawLine(2, 3, 2)
+    drawLine(3, 4, 3)
+    drawLine(5, 6, 4)
+    drawLine(6, 7, 5)
+    drawLine(1, 8, 6)
+    drawLine(8, 9, 7)
+    drawLine(9, 10, 8)
+    drawLine(1, 11, 9)
+    drawLine(11, 12, 10)
+    drawLine(12, 13, 11)
+    drawLine(0, 1, 12)
+    drawLine(0, 14, 13)
+    drawLine(15, 17, 14)
+    drawLine(0, 15, 15)
+    drawLine(0, 15, 16)
 
 
 # draw the elements transparent
